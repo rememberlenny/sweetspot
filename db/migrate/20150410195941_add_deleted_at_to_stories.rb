e35813bddc0cb4466dbedb8103e1bdb61b812125
rememberlenny <rememberlenny@gmail.com>
2015-04-10 16:04:03 -0400

    Change

--- db/migrate/20150410195941_add_deleted_at_to_stories.rb
+++ db/migrate/20150410195941_add_deleted_at_to_stories.rb
@@ -4,8 +4,6 @@ class AddDeletedAtToStories < ActiveRecord::Migration
     add_index :stories, :deleted_at
     add_column :films, :deleted_at, :datetime
     add_index :films, :deleted_at
-    add_column :photos, :deleted_at, :datetime
-    add_index :photos, :deleted_at
     add_column :users, :deleted_at, :datetime
     add_index :users, :deleted_at
     add_column :hotspots, :deleted_at, :datetime
