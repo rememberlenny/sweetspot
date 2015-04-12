cc6fa5243edfbe79a64aba3d5e9f38690a393978
rememberlenny <rememberlenny@gmail.com>
2015-04-12 13:40:59 -0400

    Add embed

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,7 +1,9 @@
 module StoriesHelper
 
   def embed_code story
-    embed_code = '<iframe src="' + path_url(@story.id) + '" frameborder="0" width="600" height="500"></iframe>'
+    embed_code =  '<!-- Begin getsweet.io embed -->'
+    embed_code += '<iframe src="' + path_url(@story.id) + '" frameborder="0" width="600" height="500"></iframe>'
+    embed_code += '<!-- End getsweet.io embed -->'
     return embed_code
   end
 
