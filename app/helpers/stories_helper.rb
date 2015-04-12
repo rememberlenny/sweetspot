5ab6716cb3142015b92e1d294c2e0519bb676ea8
rememberlenny <rememberlenny@gmail.com>
2015-04-12 13:38:48 -0400

    Create the embed code system

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,7 +1,7 @@
 module StoriesHelper
 
   def embed_code story
-    embed_code = '<iframe src="' + path_url(@story.id) + '" frameborder="0"></iframe>'
+    embed_code = '<iframe src="' + path_url(@story.id) + '" frameborder="0" width="600" height="500"></iframe>'
     return embed_code
   end
 
