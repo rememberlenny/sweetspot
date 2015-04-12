8b4f7731c64cc9534c8c9d253794d9e18e579306
rememberlenny <rememberlenny@gmail.com>
2015-04-12 18:01:31 -0400

    Change migration file

--- db/migrate/20150409183759_add_headline_byline_blurb.rb
+++ db/migrate/20150409183759_add_headline_byline_blurb.rb
@@ -2,5 +2,7 @@ class AddHeadlineBylineBlurb < ActiveRecord::Migration
   def change
     add_column :stories, :byline, :text
     add_column :stories, :blurb, :text
+    add_column :stories, :featured_photo, :integer
+    add_column :stories, :first_slide, :integer
   end
 end
--- db/migrate/20150410134837_add_featured_photo_to_stories.rb
+++
@@ -1,6 +0,0 @@
-class AddFeaturedPhotoToStories < ActiveRecord::Migration
-  def change
-    add_column :stories, :featured_photo, :integer
-    add_column :stories, :first_slide, :integer
-  end
-end
