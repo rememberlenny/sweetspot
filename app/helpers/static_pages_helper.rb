cf887b82366c408a08f6aead6a976f7d8e846059
rememberlenny <rememberlenny@gmail.com>
2015-04-13 09:31:07 -0400

    Create block for featured image

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+
+  def get_featured_photo story
+    if !story.featured_photo.nil?
+      url = attachment_url(Film.find(story.featured_photo), :image)
+      return url
+    end
+  end
 end
