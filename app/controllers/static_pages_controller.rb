41f82dee5cfa81160e07a01f117d18e16c3a4489
rememberlenny <rememberlenny@gmail.com>
2015-04-13 10:58:31 -0400

    Create the homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if current_user
       @stories = Story.live.includes(:draft).order('updated_at DESC')
       render :dash
+    else
+      @stories = Story.live
     end
   end
 end
