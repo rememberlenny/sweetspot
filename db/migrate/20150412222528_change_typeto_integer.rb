b88eba455cbd1b981f80a267bb064b59549c8b77
rememberlenny <rememberlenny@gmail.com>
2015-04-12 21:01:24 -0400

    Remove the migration

--- db/migrate/20150412222528_change_typeto_integer.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTypetoInteger < ActiveRecord::Migration
-  def change
-    remove_column :stories, :featured_photo, :string
-    remove_column :stories, :first_slide, :string
-    add_column :stories, :featured_photo, :integer
-    add_column :stories, :first_slide, :integer
-  end
-end
