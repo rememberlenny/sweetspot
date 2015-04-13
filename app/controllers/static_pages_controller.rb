6a848ee6305e53ae93ef3ea60390a1c9b5f0cb37
rememberlenny <rememberlenny@gmail.com>
2015-04-13 10:05:09 -0400

    Create an admin of some sort

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if current_user
-      @stories = Story.live.includes(:draft).order(:updated_at)
+      @stories = Story.live.includes(:draft).order('updated_at DESC')
       render :dash
     end
   end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,7 +2,6 @@ module StaticPagesHelper
 
   def get_featured_photo story
     if !story.featured_photo.nil?
-      puts 'THIS WORKS'
       url = attachment_url(Film.find(story.featured_photo), :image) + '.jpeg'
       return url
     end
