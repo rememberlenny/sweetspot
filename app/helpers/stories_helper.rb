4ead6ff197add9367371ead1e89613cd403fe45e
rememberlenny <rememberlenny@gmail.com>
2015-04-14 10:05:19 -0400

    Remove the extension without the parameter for chrome

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,12 +1,17 @@
 module StoriesHelper
 
-  def embed_code story, width = nil
+  def embed_code story, chrome = true, width = nil
+    if chrome == false
+      chrome_status = 'chromestatus=false&'
+    else
+      chrome_status = ''
+    end
     if width.nil?
       width = 600
     end
     dimensions = 'width="' + width.to_s + '"'
     embed_code =  '<!-- Begin getsweet.io embed -->'
-    embed_code += '<iframe class="embed-getsweet embed-getsweet-' + width.to_s + '" src="' + path_url(@story.id) + '" frameborder="0" ' + dimensions + ' height="500"></iframe>'
+    embed_code += '<iframe class="embed-getsweet embed-getsweet-' + width.to_s + '" src="' + path_url(@story.id) + '?' + chrome_status + ' " frameborder="0" ' + dimensions + ' height="500"></iframe>'
     embed_code += '<!-- End getsweet.io embed -->'
     return embed_code
   end
