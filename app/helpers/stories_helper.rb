75458c7119c369cb32dd488c3e994cdb092934ed
rememberlenny <rememberlenny@gmail.com>
2015-04-14 09:55:43 -0400

    Create the slides inline

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,8 +1,12 @@
 module StoriesHelper
 
-  def embed_code story
+  def embed_code story, width = nil
+    if width.nil?
+      width = 600
+    end
+    dimensions = 'width="' + width.to_s + '"'
     embed_code =  '<!-- Begin getsweet.io embed -->'
-    embed_code += '<iframe src="' + path_url(@story.id) + '" frameborder="0" width="600" height="500"></iframe>'
+    embed_code += '<iframe class="embed-getsweet embed-getsweet-' + width.to_s + '" src="' + path_url(@story.id) + '" frameborder="0" ' + dimensions + ' height="500"></iframe>'
     embed_code += '<!-- End getsweet.io embed -->'
     return embed_code
   end
