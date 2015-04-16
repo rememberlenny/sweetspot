2bf601297c630d660b871b4352d421bfd67720fd
rememberlenny <rememberlenny@gmail.com>
2015-04-16 08:52:02 -0400

    Update the content for user update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,7 +19,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         set_flash_message :notice, flash_key
       end
       sign_in resource_name, resource, bypass: true
-      respond_with resource, location: after_update_path_for(resource)
+      respond_with resource, location: edit_user_registration_path
     else
       clean_up_passwords resource
       respond_with resource
