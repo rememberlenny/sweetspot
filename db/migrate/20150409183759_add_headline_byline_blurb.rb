37f158ed81ec3ec160951bdda69084ce96a84e1b
rememberlenny <rememberlenny@gmail.com>
2015-04-16 09:51:24 -0400

    Remove ruby blurb from migration

--- db/migrate/20150409183759_add_headline_byline_blurb.rb
+++ db/migrate/20150409183759_add_headline_byline_blurb.rb
@@ -6,17 +6,3 @@ class AddHeadlineBylineBlurb < ActiveRecord::Migration
     add_column :stories, :first_slide, :integer
   end
 end
-
-
-ss = Story.all
-ss.each do |s|
-  if ! s.featured_photo.nil?
-    puts 'Found featured_photo non-nil'
-    s.featured_photo = s.featured_photo.to_i
-  end
-  if ! s.first_slide.nil?
-    puts 'Found featured_slide non-nil'
-    s.first_slide = s.first_slide.to_i
-  end
-  s.save
-end
