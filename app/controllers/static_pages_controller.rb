4148786306b7f9c639e49d6fe78d8252f9c73f74
rememberlenny <rememberlenny@gmail.com>
2015-04-16 20:03:38 -0400

    Create sweetspot photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @featured_stories = Story.where(featured_story: true)
     if current_user
       @stories = Story.live.where(:user_id => current_user.id).includes(:draft).order('updated_at DESC')
       render :dash
     else
       @stories = Story.live
-      @featured_stories = Story.where(featured_story: true).limit(6)
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
 module ApplicationHelper
-  def featured_stories
-    # @featured_stories = Story.all.where(featured_story: true).limit(6)
-  end
+
 end
