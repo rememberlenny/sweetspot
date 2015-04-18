abe08b85ff0fdf0c991139105a80918f399db76b
rememberlenny <rememberlenny@gmail.com>
2015-04-18 04:06:32 -0400

    Update the boolean

--- db/migrate/20150416163952_add_featured_stories_to_users.rb
+++ db/migrate/20150416163952_add_featured_stories_to_users.rb
@@ -1,6 +1,5 @@
 class AddFeaturedStoriesToUsers < ActiveRecord::Migration
   def change
-    remove_column :stories, :featured_story, :boolean
     add_column :stories, :featured_story, :boolean, default: false
   end
 end
