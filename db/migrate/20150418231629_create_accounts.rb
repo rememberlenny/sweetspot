d4527566e9061b46045b01e09270f7f7b729fa91
rememberlenny <rememberlenny@gmail.com>
2015-04-18 19:18:22 -0400

    Create the account conditions for the subdomain

--- db/migrate/20150418231629_create_accounts.rb
+++ db/migrate/20150418231629_create_accounts.rb
@@ -3,8 +3,8 @@ class CreateAccounts < ActiveRecord::Migration
     create_table :accounts do |t|
       t.string :subdomain
       t.integer :user_id
-
       t.timestamps null: false
     end
+    add_index  :accounts, :subdomain, unique: true
   end
 end
--- db/migrate/20150418231644_add_stories_to_account.rb
+++ db/migrate/20150418231644_add_stories_to_account.rb
@@ -1,5 +1,6 @@
 class AddStoriesToAccount < ActiveRecord::Migration
   def change
+    remove_column :users, :username
     add_column :stories, :account_id, :integer
   end
 end
