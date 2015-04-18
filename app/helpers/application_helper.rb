d7f0dcaba674d93629847dbbf8eff3420fdb6af0
rememberlenny <rememberlenny@gmail.com>
2015-04-18 00:17:45 -0400

    Create the pay tiers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
 
+  def story_count tier
+    if tier == 'free'
+      return '1'
+    elsif tier == 'basic'
+      return '3'
+    elsif tier == 'pro'
+      return 'Unlimited'
+    elsif tier == 'organization'
+      return 'Unlimited'
+    end
+  end
 end
