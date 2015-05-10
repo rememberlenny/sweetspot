b4fc771cd6335d9cb29ea01b23c9bc81ea1e8a2e
rememberlenny <rememberlenny@gmail.com>
2015-05-10 14:18:50 -0400

    Moving refile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,3 @@
-aws = {
-  access_key_id: ENV['S3_RE_KEY'],
-  secret_access_key: ENV['S3_RE_SECRET'],
-  bucket: ENV['S3_RE_BUCKET'],
-}
-Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
-
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
